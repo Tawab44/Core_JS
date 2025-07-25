c0aa50fbf35523c8835099a7ebeda1c31792349a
Tawab44 <tawabshaikh44445@gmaiul.com>
2025-07-25 08:14:07 +0530

    core

---
+++ try.js
@@ -0,0 +1,59 @@
+// setTimeout(()=>console.log("Hello"),2000)
+
+// const promise = new Promise((resolve,reject)=>
+// {
+//     const success=true;
+//     setTimeout(()=>{if (success){
+//         resolve(console.log("Promise settled"))
+//     }else
+//     {
+//         reject(console.log("Promise rejected"))
+//     }
+// }
+
+// )})//
+
+//8/07/2025
+let name="Tawab";
+let age=21;
+console.log("Hello " + name +",your age is " + age);
+age=17
+if (age>=18) {
+    console.log("You can vote");
+}else{
+    console.log("You cannot vote");
+}
+console.log("Hello " + name +",your age is " + age);
+function greet(){
+    return console.log("Hello Coder");
+}
+
+function thunder(age){
+    return console.log("U r " +age+ " years old.");
+}
+thunder(21);
+function add(a,b){
+    console.log("The additiopn is ",a+b)
+}
+add(5,5);
+function div(a,b){
+    console.log("The division is ",a/b)
+}
+div(5,5);
+function Tawab(a,b){
+    console.log("This is multiplication",a*b);
+}
+Tawab(5,5);
+
+function sub(a,b){
+    console.log("The subtraction is",a-b);
+
+}
+sub(5,0);
+
+let user={
+    name1:"user1",
+    age:21
+
+};
+console.log(user.name1);
