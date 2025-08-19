f97d8d76f83d76ff259286fce5d6c68e3335b6dc
Tawab44 <tawabshaikh14444@gmail.com>
2025-08-19 14:36:54 +0530

    increasing js

--- main.js
+++ main.js
@@ -1,3 +1,170 @@
 console.log("Hello world!")
 //that was 5 months ago today is 18/08/25
 //19/08/25
+
+setTimeout(()=>console.log("Hello"),2000)
+
+{const promise = new Promise((resolve,reject)=>
+{
+    const success=true;
+    setTimeout(()=>{if (success){
+        resolve(console.log("Promise settled"))
+    }else
+    {
+        reject(console.log("Promise rejected"))
+    }
+}
+
+)})}//
+
+//8/07/2025
+let name="Tawab";
+let age=21;
+console.log("Hello " + name +",your age is " + age);
+age=17
+if (age>=18) {
+    console.log("You can vote");
+}else{
+    console.log("You cannot vote");
+}
+console.log("Hello " + name +",your age is " + age);
+function greet(){
+    return console.log("Hello Coder");
+}
+
+function thunder(age){
+    return console.log("U r " +age+ " years old.");
+}
+thunder(21);
+function add(a,b){
+    console.log("The additiopn is ",a+b)
+}
+add(5,5);
+function div(a,b){
+    console.log("The division is ",a/b)
+}
+div(5,5);
+function Tawab(a,b){
+    console.log("This is multiplication",a*b);
+}
+Tawab(5,5);
+
+function sub(a,b){
+    console.log("The subtraction is",a-b);
+
+}
+sub(5,0);
+
+let user={
+    name1:"user1",
+    age:21
+
+};
+console.log(user.name1);
+
+setTimeout(()=>console.log("Hello"),2000)
+
+const promise = new Promise((resolve,reject)=>
+{
+    const success=true;
+    setTimeout(()=>{if (success){
+        resolve(console.log("Promise settled"))
+    }else
+    {
+        reject(console.log("Promise rejected"))
+    }
+}
+
+)})//
+
+//8/07/2025
+//again all just to test commit
+
+let name1="Tawab";
+let age1=21;
+console.log("Hello " + name1 +",your age is " + age1);
+age=17
+if (age>=18) {
+    console.log("You can vote");
+}else{
+    console.log("You cannot vote");
+}
+console.log("Hello " + name1 +",your age is " + age);
+function greet(){
+    return console.log("Hello Coder");
+}
+
+function thunder(age){
+    return console.log("U r " +age+ " years old.");
+}
+thunder(21);
+function add(a,b){
+    console.log("The additiopn is ",a+b)
+}
+add(5,5);
+function div(a,b){
+    console.log("The division is ",a/b)
+}
+div(5,5);
+function Tawab(a,b){
+    console.log("This is multiplication",a*b);
+}
+Tawab(5,5);
+
+function sub(a,b){
+    console.log("The subtraction is",a-b);
+
+}
+sub(5,0);
+
+let user1={
+    name1:"user1",
+    age:21
+
+};
+console.log(user1.name1);
+//18 august  
+let name2="Tawab";
+let age2=21;
+console.log("Hello " + name2 +",your age is " + age2);
+age=17
+if (age>=18) {
+    console.log("You can vote");
+}else{
+    console.log("You cannot vote");
+}
+console.log("Hello " + name2 +",your age is " + age);
+function greet(){
+    return console.log("Hello Coder");
+}
+
+function thunder(age){
+    return console.log("U r " +age+ " years old.");
+}
+thunder(21);
+function add(a,b){
+    console.log("The additiopn is ",a+b)
+}
+add(5,5);
+function div(a,b){
+    console.log("The division is ",a/b)
+}
+div(5,5);
+function Tawab(a,b){
+    console.log("This is multiplication",a*b);
+}
+Tawab(5,5);
+
+function sub(a,b){
+    console.log("The subtraction is",a-b);
+
+}
+sub(5,0);
+
+let user2={
+    name1:"user1",
+    age:21
+
+};
+console.log(user2.name1);
+
